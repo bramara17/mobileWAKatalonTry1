c1ec4d96b2b333314d4e85b313b89783374dc4c8
Thinkpad Bram <bramara.konsultan@work.bri.co.id>
2024-02-16 15:35:59 +0700

    upload project

---
+++ Test Suites/Welcome WA.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Welcome WA</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f9ed0c35-e194-4a3c-b362-52ff438c3dd5</testSuiteGuid>
+   <testCaseLink>
+      <guid>7d0a968d-c09f-4a7e-af48-e5c107db0aee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Welcome Whatsapp</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
